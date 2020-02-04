467d801666a3fa7e94be46bc2ddd6dac7ae0db3c
github username <github email address>
2020-02-04 08:57:33 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,23 @@
 class StudentsController < ApplicationController
+  before_action :set_student, only: :show
   
   def index
     @students = Student.all
   end
 
   def show
-    @student = Student.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    session[:form_params] = params.inspect
+    redirect_to new_student_path
+  end
+
+  private #everything below this line = helper method or method that doesn't render shit via views.
+    def set_student
+      @student = Student.find(params[:id])
+    end
 end
